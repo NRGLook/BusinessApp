8007491be74ecc508ea6bc9f5aff0c66ff982d97
Илья Тиханенок <ilya.tsikhanionak@192.168.100.7>
2025-05-01 14:37:13 +0300

    stage version with base home page

--- business-app-back/main.py
+++ business-app-back/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 
+from starlette.middleware.cors import CORSMiddleware
+
 from src.api.routes.businesses.view import business_router
+from src.api.routes.home.views import home_router
 
 app = FastAPI()
-app.include_router(business_router)
 
-@app.get("/home")
-async def home_page(username: str):
-    return f"Dima I need to write dimploma right now {username}"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем все origins (для разработки)
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все методы
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
+
+app.include_router(business_router)
+app.include_router(home_router)
